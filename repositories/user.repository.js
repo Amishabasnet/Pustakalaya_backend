2cffae49e02f238a5eabd3c66a84d6462e16e2f8
Amisha Basnet <amishaaaaaa045@gmail.com>
2026-06-03 23:41:23 +0545

    code update

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -1,11 +1,13 @@
 const User = require("../models/User");
 
 class UserRepository {
-  findById(id)          { return User.findById(id); }
-  findByEmail(email)    { return User.findOne({ email }); }
+  findById(id)               { return User.findById(id); }
+  findByIdWithPass(id)       { return User.findById(id).select("+password"); }
+  findByEmail(email)         { return User.findOne({ email }); }
   findByEmailWithPass(email) { return User.findOne({ email }).select("+password"); }
-  create(data)          { return User.create(data); }
-  updateById(id, data)  { return User.findByIdAndUpdate(id, data, { new: true }); }
+  findByUsername(username)   { return User.findOne({ username }); }
+  create(data)               { return User.create(data); }
+  updateById(id, data)       { return User.findByIdAndUpdate(id, data, { new: true }); }
 }
 
 module.exports = new UserRepository();
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,14 @@
-const router          = require("express").Router();
-const authController  = require("../controllers/auth.controller");
-const { protect }     = require("../middlewares/auth.middleware");
-const { signUpValidator, signInValidator } = require("../validators/auth.validator");
+const router         = require("express").Router();
+const authController = require("../controllers/auth.controller");
+const { protect }    = require("../middlewares/auth.middleware");
+const { signUpValidator, signInValidator, updateProfileValidator } = require("../validators/auth.validator");
 
-router.post("/signup",  signUpValidator, (req, res, next) => authController.signUp(req, res, next));
-router.post("/signin",  signInValidator, (req, res, next) => authController.signIn(req, res, next));
-router.post("/signout",                 (req, res, next) => authController.signOut(req, res, next));
-router.post("/refresh",                 (req, res, next) => authController.refresh(req, res, next));
-router.get ("/me",      protect,        (req, res, next) => authController.getMe(req, res, next));
+router.post("/signup",  signUpValidator,           (req, res, next) => authController.signUp(req, res, next));
+router.post("/signin",  signInValidator,            (req, res, next) => authController.signIn(req, res, next));
+router.post("/signout",                             (req, res, next) => authController.signOut(req, res, next));
+router.post("/refresh",                             (req, res, next) => authController.refresh(req, res, next));
+router.get ("/me",      protect,                   (req, res, next) => authController.getMe(req, res, next));
+router.patch("/me",     protect, updateProfileValidator, (req, res, next) => authController.updateProfile(req, res, next));
+router.get ("/me/stats", protect,                  (req, res, next) => authController.getProfileStats(req, res, next));
 
 module.exports = router;
