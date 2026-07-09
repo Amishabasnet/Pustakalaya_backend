0962f276948620e1af5bdae98b17d1366f92689e
Amisha Basnet <amishaaaaaa045@gmail.com>
2026-07-09 14:19:09 +0545

    repo update

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -8,6 +8,13 @@ class UserRepository {
   findByUsername(username)   { return User.findOne({ username }); }
   create(data)               { return User.create(data); }
   updateById(id, data)       { return User.findByIdAndUpdate(id, data, { new: true }); }
+
+  // Admin
+  findAllPaginated({ query, skip, limit }) {
+    return User.find(query).sort({ createdAt: -1 }).skip(skip).limit(limit);
+  }
+
+  countAllFiltered(query) { return User.countDocuments(query); }
 }
 
 module.exports = new UserRepository();
